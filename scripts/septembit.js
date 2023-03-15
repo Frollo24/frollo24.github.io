3e3fa04488b57d5849dd1f445cc5acd0c985c36c
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2023-03-15 17:16:37 +0100

    Make septembit pixelart show on

--- scripts/septembit.js
+++ scripts/septembit.js
@@ -1,19 +1,19 @@
-import pixelart from '../resources/pixelart.json' assert { type : "json" };
-const { default: pixelart } = await import('../resources/pixelart.json', { assert: { type : "json" } } );
-
-const figures = document.getElementById("septembit");
-
-var septembit = pixelart.septembit;
-
-for (let i = 0; i < septembit.length; i++) {
-  figures.innerHTML += `
-
-  <figure>
+$.getJSON('../resources/pixelart.json', function(json){
+  const pixelart = json;
+  const figures = document.getElementById("septembit");
+  
+  var septembit = pixelart.septembit;
+  
+  for (let i = 0; i < septembit.length; i++) {
+    figures.innerHTML += `
+    
+    <figure>
     <a href="/images/pixelart/septembit/${septembit[i].number}.png" target="_blank">
-      <img src="/images/pixelart/septembit/${septembit[i].number}.png" alt="${septembit[i].name}">
+    <img src="/images/pixelart/septembit/${septembit[i].number}.png" alt="${septembit[i].name}">
     </a>
     <figcaption>${septembit[i].name}</figcaption>
-  </figure>
-
-  `;
-}
+    </figure>
+    
+    `;
+  }
+})
