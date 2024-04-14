55225e402e43ff4b257d824c21a8cbdab6c2aca2
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2024-04-14 21:42:42 +0200

    Basic portfolio structure created

---
+++ scripts/headerload.js
@@ -0,0 +1,23 @@
+var header = document.getElementById("header");
+
+header.innerHTML = `
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/projects/">Projects</a></li>
+    <li><a href="/art/">Art</a></li>
+    <li><a href="/about.html">About</a></li>
+    <li><a href="/contact.html">Contact</a></li>
+  </ul>
+</nav>
+`;
+
+
+
+var footer = document.getElementById("footer");
+
+footer.innerHTML = `
+<div id="author">
+  <a href="/about.html">Adrián Salgado Jimeno</a>
+</div>
+`;
