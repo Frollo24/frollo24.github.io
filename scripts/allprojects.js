4024c3ab75ee9573fd9352baaf7b20e6e4f320fa
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2024-04-18 01:33:34 +0200

    Added all projects page text

---
+++ scripts/allprojects.js
@@ -0,0 +1,50 @@
+$.getJSON('../resources/projects.json', function(json){
+  function insertProjects(projectListName){
+    var projectList = json[projectListName];
+    var projectSection = document.getElementById(projectListName);
+    var projectAmount = projectList.length;
+
+    for (let i = 0; i < projectAmount; i++) {
+      if (i % 2 == 0){
+        projectSection.innerHTML += `
+        <div class="project">
+          <figure>
+            <a href="${projectList[i].viewmoreSrc}">
+              <img src="${projectList[i].imageFolder}01.png" alt="${projectList[i].title}">
+            </a>
+          </figure>
+          <article class="project-text">
+            <h1>${projectList[i].title}</h1>
+            <p>
+              ${projectList[i].summary}
+            </p>
+            <br>
+            <a class="viewmore" href="${projectList[i].viewmoreSrc}" target="_blank">${projectList[i].viewmoreText}</a>
+          </article>
+        </div>
+        `;
+      } else {
+        projectSection.innerHTML += `
+        <div class="project">
+          <article class="project-text">
+            <h1>${projectList[i].title}</h1>
+            <p>
+              ${projectList[i].summary}
+            </p>
+            <br>
+            <a class="viewmore" href="${projectList[i].viewmoreSrc}" target="_blank">${projectList[i].viewmoreText}</a>
+          </article>
+          <figure>
+            <a href="${projectList[i].viewmoreSrc}">
+              <img src="${projectList[i].imageFolder}01.png" alt="${projectList[i].title}">
+            </a>
+          </figure>
+        </div>
+        `;
+      }
+    }
+  }
+  
+  insertProjects("computergraphics");
+  insertProjects("videogames");
+});
